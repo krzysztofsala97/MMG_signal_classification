34fd7dc2fbf371c3dd0a31407da163eefdd5ffc8
krzysztofsala97 <krzysztof.sala97@gmail.com>
2021-02-07 16:18:11 +0100

    Calculating ellipsoid parameters

--- main.py
+++ main.py
@@ -67,20 +67,45 @@ df_mmg_calib_Zd = df_mmg_calib_Zd[df_mmg_calib_Zd["ID"] == 0]
 # print(f'Accelerometer offset for axis Z of sensor 0 is {calculate_offset(df_mmg_calib_Zu, df_mmg_calib_Zd,0,1,"Z")}')
 # ----------------------------------------------------------------------------------------------------------------------
 # Calculating gyroscope and accelerometer offset and ellipsoid parameters
+saved_data = {}
 for i in range(0, 8):
     print("-"*15)
-    print(f'Gyroscope offset for axis X of sensor {i} is {calculate_offset(df_mmg_calib_Xu, df_mmg_calib_Xd, i, 0, "X")}')
-    print(f'Gyroscope offset for axis Y of sensor {i} is {calculate_offset(df_mmg_calib_Yu, df_mmg_calib_Yd, i, 0, "Y")}')
-    print(f'Gyroscope offset for axis Z of sensor {i} is {calculate_offset(df_mmg_calib_Zu, df_mmg_calib_Zd, i, 0, "Z")}')
-    print(f'Accelerometer offset for axis X of sensor {i} is {calculate_offset(df_mmg_calib_Xu, df_mmg_calib_Xd, i, 1, "X")}')
-    print(f'Accelerometer offset for axis Y of sensor {i} is {calculate_offset(df_mmg_calib_Yu, df_mmg_calib_Yd, i, 1, "Y")}')
-    print(f'Accelerometer offset for axis Z of sensor {i} is {calculate_offset(df_mmg_calib_Zu, df_mmg_calib_Zd, i, 1, "Z")}')
-
-    res = fmin_bfgs(elipsoid, np.array([1, 1, 1, 0, 0, 0]), args=(df_mmg_calib[["Mag_"+str(i)+"_X",
-                                                                                "Mag_"+str(i)+"_Y",
-                                                                                "Mag_"+str(i)+"_Z"]],))
+    g_x = calculate_offset(df_mmg_calib_Xu, df_mmg_calib_Xd, i, 0, "X")
+    g_y = calculate_offset(df_mmg_calib_Yu, df_mmg_calib_Yd, i, 0, "Y")
+    g_z = calculate_offset(df_mmg_calib_Zu, df_mmg_calib_Zd, i, 0, "Z")
+    a_x = calculate_offset(df_mmg_calib_Xu, df_mmg_calib_Xd, i, 1, "X")
+    a_y = calculate_offset(df_mmg_calib_Yu, df_mmg_calib_Yd, i, 1, "Y")
+    a_z = calculate_offset(df_mmg_calib_Zu, df_mmg_calib_Zd, i, 1, "Z")
+    res = fmin_bfgs(elipsoid, np.array([1, 1, 1, 0, 0, 0]), args=(df_mmg_calib[["Mag_" + str(i) + "_X",
+                                                                                "Mag_" + str(i) + "_Y",
+                                                                                "Mag_" + str(i) + "_Z"]],))
+    print(f'Gyroscope offset for axis X of sensor {i} is {g_x}')
+    print(f'Gyroscope offset for axis Y of sensor {i} is {g_y}')
+    print(f'Gyroscope offset for axis Z of sensor {i} is {g_z}')
+    print(f'Accelerometer offset for axis X of sensor {i} is {a_x}')
+    print(f'Accelerometer offset for axis Y of sensor {i} is {a_y}')
+    print(f'Accelerometer offset for axis Z of sensor {i} is {a_z}')
     print(f'Ellipsoid parameters for sensor{i}: a={res[0]}; b={res[1]}; c={res[2]}; x0={res[3]}, y0={res[4]}; z0={res[5]}')
+
+    saved_data[i] = {}
+    saved_data[i]["gyro"] = {}
+    saved_data[i]["acc"] = {}
+    saved_data[i]["mag"] = {}
+    saved_data[i]["gyro"]["X"] = g_x
+    saved_data[i]["gyro"]["Y"] = g_y
+    saved_data[i]["gyro"]["Z"] = g_z
+    saved_data[i]["acc"]["X"] = a_x
+    saved_data[i]["acc"]["Y"] = a_y
+    saved_data[i]["acc"]["Z"] = a_z
+    saved_data[i]["mag"]["a"] = res[0]
+    saved_data[i]["mag"]["b"] = res[1]
+    saved_data[i]["mag"]["c"] = res[2]
+    saved_data[i]["mag"]["x0"] = res[3]
+    saved_data[i]["mag"]["y0"] = res[4]
+    saved_data[i]["mag"]["z0"] = res[5]
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 # TODO: Use calculated parameters to calibrate sensors data
-
+# print("-"*15)
+# print(saved_data)
